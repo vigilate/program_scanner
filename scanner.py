f193ee2c89f6778e93f314599e94948ea559f59e
ampotos <ampotos@gmail.com>
2015-12-14 18:40:09 +0000

    test

--- scanner.py
+++ scanner.py
@@ -4,15 +4,28 @@ import requests
 import json
 
 # this config var will be move with all the config of this scanner
-url_backend = "127.0.0.1"
+url_backend = "http://127.0.0.1:8000/api/uprog/submit_programs/"
+user = "vigilate"
+mdp = "vigilate"
 
 # prog_list has to be a list like that :
 # [{"program" : "program1_name", "version" : "version_prog1"},
 #  {"program" : "program2_name", "version" : "version_prog2"}, ...]
-def send_datas(prog_list):
-    data = json.dumps(prog_list)
-    r = requests.post(url_backend, data)
-
+def send_data(prog_list):
+    data = json.dumps({"programs_list" : prog_list})
+#    headers = {'content-type': 'application/json'}
+    headers = {'Accept': 'application/json; indent=4', 'content-type': 'application/x-www-form-urlencoded'}
+    print(data)
+    r = requests.post(url_backend, data=data, auth=(user, mdp), headers=headers)
+    print(r.text)
     if not r.ok:
         return False
     return True
+
+
+progs = [{"program_name" : "program1_name", "program_version" : "version_prog1"},
+         {"program_name" : "program2_name", "program_version" : "version_prog2"},
+         {"program_name" : "program3_name", "program_version" : "version_prog3"},
+         {"program_name" : "program4_name", "program_version" : "version_prog4"}]
+
+send_data(progs)
