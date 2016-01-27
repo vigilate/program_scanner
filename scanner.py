455e6d3607c1b9b42aef7ac0c9a74f0436a7c3df
Vigilate <vigilate_2017@labeip.epitech.eu>
2016-01-27 17:12:39 +0000

    get list of installed package for dpkg based distribution

--- scanner.py
+++ scanner.py
@@ -2,6 +2,8 @@
 
 import requests
 import json
+import subprocess
+import platform
 
 # this config var will be move with all the config of this scanner
 url_backend = "http://127.0.0.1/api/uprog/submit_programs/"
@@ -13,7 +15,6 @@ mdp = "vigilate"
 #  {"program" : "program2_name", "version" : "version_prog2"}, ...]
 def send_data(prog_list):
     data = json.dumps({"programs_list" : prog_list})
-#    headers = {'content-type': 'application/json'}
     headers = {'Accept': 'application/json; indent=4', 'content-type': 'application/x-www-form-urlencoded'}
     print(data)
     r = requests.post(url_backend, data="query="+data, auth=(user, mdp), headers=headers)
@@ -22,10 +23,51 @@ def send_data(prog_list):
         return False
     return True
 
+def get_pacman_progs():
+    #pacman -Q
+    return []
 
-progs = [{"program_name" : "program1_name", "program_version" : "version_prog1"},
-         {"program_name" : "program2_name", "program_version" : "version_prog2"},
-         {"program_name" : "program3_name", "program_version" : "version_prog3"},
-         {"program_name" : "program4_name", "program_version" : "version_prog4"}]
+def get_pkg_progs():
+    #pkg_info
+    return []
 
-send_data(progs)
+def get_dpkg_progs():
+    try:
+        p = subprocess.run(['dpkg', '-l'], stdout=subprocess.PIPE)
+    except FileNotFoundError:
+        return []
+
+    output = p.stdout.decode().split('\n')
+    while not output[0].endswith("==="):
+        output.pop(0)
+
+    progs = []
+    for l in output[1:-1]:
+        prog = list(filter(None, l.split(' ')))
+        progs.append({"program_name" : prog[1], "program_version" : prog[2]})
+        
+    return progs
+
+def get_rpm_progs():
+    #rpm -qa
+    return []
+
+def main():
+    progs = {"programs_list" : []}
+
+    if platform.system() == "Linux":
+        progs["programs_list"] += get_pacman_progs()
+        progs["programs_list"] += get_pkg_progs()
+        progs["programs_list"] += get_dpkg_progs()
+        progs["programs_list"] += get_rpm_progs()
+    print(send_data(progs))
+
+if __name__ == '__main__':
+    main()
+
+# progs = [{"program_name" : "program1_name", "program_version" : "version_prog1"},
+#          {"program_name" : "program2_name", "program_version" : "version_prog2"},
+#          {"program_name" : "program3_name", "program_version" : "version_prog3"},
+#          {"program_name" : "program4_name", "program_version" : "version_prog4"}]
+
+# send_data(progs)
